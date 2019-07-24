8c8084996a3480f61edcf1a146a1aa21c04d5b0b
DerekChristy <kered.christy@gmail.com>
2019-07-25 00:01:29 +0530

    added feedback page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'neurowaves.feedback@gmail.com',
+    pass: 'radhika123'
+  }
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Positive Waves' });
@@ -26,6 +38,25 @@ router.get('/contact', (req, res) => {
 
 router.get('/services', (req, res) => {
   res.render('pages/services', {title: 'service'})
+})
+
+router.post('/feedback', (req, res) => {
+  console.log(req.body)
+  var mailOptions = {
+    from: 'neurowaves.feedback@gmail.com',
+    to: 'kered.christy@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: req.body
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  })
+
+  res.redirect('/');
 })
 
 module.exports = router;
